2810ca306b537464b1950c40840055c5c9d2ffe7
yohanLeonardi <dalmyo1011@gmail.com>
2017-12-24 12:28:50 +0900

    created new file boardmanager.js and applied it
    - modulized initBoard to boardmanager.js
    - applied boardManager.init method instead of initBoard

--- js/index.js
+++ js/index.js
@@ -1,31 +1,6 @@
 function init() {
-  initBoard();
+  boardManager.init();
   chessPieceManager.init();
-}
-
-function initBoard() {
-  var board = $('#board');
-  for (var i = 0; i < 8; i++) {
-    board.append(initRow(8-i));
-  }
-}
-
-function initRow(index) {
-  return $( '<div/>', {
-      html: initSquares(index),
-      'class': 'row',
-      'id': 'row'+index
-  });
-}
-
-function initSquares(index) {
-  var columnNames = 'abcdefgh';
-  var squares = '';
-  for (var i = 0; i < 8; i++) {
-    squares += '<div class="square" id='+columnNames.charAt(i)+(index)+'>'
-            + '</div>';
-  }
-  return squares;
 }
 
 window.onload = init;
