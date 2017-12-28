c7d053f7af46974de080b1ba75801143a1261279
yohanLeonardi <dalmyo1011@gmail.com>
2017-12-28 13:15:54 +0900

    modified chesspiece class
    - emphasized private class members by adding underscore('_') in front of all the private variables
    - added color on div class for specifying color types (it may or may not be used on future)

--- js/chesspiece.js
+++ js/chesspiece.js
@@ -1,31 +1,50 @@
 class ChessPiece {
   constructor(type, id, color, position, imgSrc) {
-    this.type = type;
-    this.id = this.type+'_'+id;
-    this.color = color;
-    this.imgSrc = imgSrc;
-    this.position = position;
-    this.moved = false;
+    this._type = type;
+    this._color = color;
+    this._id = this._color+'_'+this._type+'_'+id;
+    this._imgSrc = imgSrc;
+    this._position = position;
+    this._moved = false;
+    this._taken = false;
   }
 
   init() {
-    var obj = '<div class="piece" id = "'+this.color+'_'+this.id+'" draggable="true">'
-            + '<img src="./img/chesspieces/'+this.imgSrc+'" alt="Chess piece" draggable="false">'
+    var obj = '<div class="piece ' + this._color + '" id = "'+this._id+'" draggable="true">'
+            + '<img src="./img/chesspieces/'+this._imgSrc+'" alt="Chess piece" draggable="false">'
             + '</div>';
-    $('#'+this.position).append(obj);
+    $('#'+this._position).append(obj);
   }
 
   set(destination) {
-    if (this.position === destination) return;
-    if (this.moved === true) {
+    if (this._position === destination) return;
+    if (this._moved === true) {
       //TODO: set piece to move with only one cell (diagonal when there's target on diagonal way)
     }
-    this.position = destination;
-    this.moved = true;
+    this._position = destination;
+    this._moved = true;
   }
 
-  getId() {
-    return id;
+  get id() {
+    return this._id;
+  }
+
+  get position() {
+    return this._position;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get taken() {
+    return this._taken;
+  }
+
+  set taken(val) {
+    //TODO: take the obj to taken div
+    $('#'+this._id).appendTo('#taken_'+this._color);
+    return this._taken = !this._taken;
   }
 }
 
